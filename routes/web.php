93ba43b43fc2154f21c71ebae00d23a8f27e9e7b
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2024-01-21 06:41:13 -0300

    Created routes of member, time, team amd settings

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterMemberController;
+use App\Http\Controllers\MembersController as MembersControllerAlias;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\TeamsController;
+use App\Http\Controllers\TimesController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -29,10 +34,20 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(callback: function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/members/register', [RegisterMemberController::class, 'register'])->name('member.register');
+
+    Route::resource('/teams', TeamsController::class);
+
+    Route::resource('/times', TimesController::class);
+
+    Route::resource('/settings', SettingsController::class);
+
+    Route::resource('/members', MembersControllerAlias::class);
 });
 
 require __DIR__.'/auth.php';
