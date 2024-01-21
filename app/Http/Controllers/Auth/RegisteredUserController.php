d81095e718f6223a608171f041bf3b0586cb8bff
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2024-01-21 06:40:01 -0300

    refactor

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,7 +38,6 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
